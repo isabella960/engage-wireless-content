d42abe9f2f49fca58450c9fdfa515f09922e019e
Isabella <143600689+isabella960@users.noreply.github.com>
2023-10-08 01:36:51 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+}
+    }
+        }
+        }
+        for (int i = 0; i < 10; i++) {
+
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
