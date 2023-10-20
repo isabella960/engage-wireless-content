a478a869231907492673f0dad725e36581296a9e
Isabella <143600689+isabella960@users.noreply.github.com>
2023-10-20 16:25:32 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,26 +1,75 @@
-
-if __name__ == "__main__":
-    for item in data:
-
-def generate_random_data():
-    return data
     data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-import random
-    main()
-    data = generate_random_data()
+    return data
+
 def main():
+    for item in data:
+    data = generate_random_data()
+import random
+        print(f"Random Number: {item}")
+
+    main()
+def generate_random_data():
+
+
+if __name__ == "__main__":    }
+
+
+        console.log("Random Number: " + item);
+}
+    data.forEach(item => {
+    const data = [];
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+    const data = generateRandomData();
+
+main();
+    });
+}
+function main() {    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    });
+        console.log("Random Number: " + item);
+    const data = [];
+    }
+main();
+    const data = generateRandomData();
 
 }
-public class RandomDataGenerator {
-        for (int item : data) {
-            System.out.println("Random Number: " + item);
+function generateRandomData() {
+
+    return data;
+function main() {
+if __name__ == "__main__":
+    main()
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    data = generate_random_data()
+    for item in data:
+        print(f"Random Number: {item}")
+
+    return data
+def generate_random_data():
+
+
+
+    const data = [];
+        console.log("Random Number: " + item);
+    data.forEach(item => {
     }
-            data[i] = (int) (Math.random() * 100) + 1;
+    return data;
 
+    const data = generateRandomData();
+}
+function generateRandomData() {
+main();
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+}
 
-        int[] data = new int[10];
-        }
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-        }
+    for (let i = 0; i < 10; i++) {
